95c707fd53413d42a90bf8e757e03cc07136872b
Sam Whitehead <sam.everythingcomputers@gmail.com>
2021-07-02 18:34:57 +0100

    Initial commit

---
+++ vim_templates/template.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* <FILENAME>
+ * Copyright Sam Whitehead, <YEAR>
+ * Last updated <DATE>
+ */
+
+int main(int argc, char **argv)
+{
+    printf("Hello from <FILENAME>!\n");
+    return 0;
+}
